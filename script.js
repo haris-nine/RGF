447fcf28ba71af18ea12e5b035356ae05bbdf566
harry <87892644+haris-nine@users.noreply.github.com>
2024-10-25 00:30:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
 var button = document.getElementById('right');
 button.onclick = function () {
-    document.getElementById('box-container').scrollLeft += 200;
+    document.getElementById('box-container').scrollLeft += 300;
 };
 
 var back = document.getElementById('left');
 back.onclick = function () {
-    document.getElementById('box-container').scrollLeft -= 200;
+    document.getElementById('box-container').scrollLeft -= 300;
 };
 
 var ibutton = document.getElementById('i-right');
 ibutton.onclick = function () {
-    document.getElementById('i-cards').scrollLeft += 200;
+    document.getElementById('i-cards').scrollLeft += 300;
 };
 
 var iback = document.getElementById('i-left');
 iback.onclick = function () {
-    document.getElementById('i-cards').scrollLeft -= 200;
+    document.getElementById('i-cards').scrollLeft -= 300;
 };
 
