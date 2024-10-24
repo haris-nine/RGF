f11264bc05028c43095cb58161d81ea32371e515
harry <87892644+haris-nine@users.noreply.github.com>
2024-10-24 19:28:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ var back = document.getElementById('left');
 back.onclick = function () {
     document.getElementById('box-container').scrollLeft -= 200;
 };
+
+var ibutton = document.getElementById('i-right');
+ibutton.onclick = function () {
+    document.getElementById('i-cards').scrollLeft += 200;
+};
+
+var iback = document.getElementById('i-left');
+iback.onclick = function () {
+    document.getElementById('i-cards').scrollLeft -= 200;
+};
+
